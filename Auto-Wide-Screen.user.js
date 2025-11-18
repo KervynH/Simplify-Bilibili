d9aee5b4c1cad44815f36b9398320a478e17c85e
KervynH <123544684+KervynH@users.noreply.github.com>
2025-11-18 17:47:51 -0500

    Upload auto-wide-screen.user.js
    Updated namespace and description for clarity.

---
+++ Auto-Wide-Screen.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Bilibili Auto Wide Screen
+// @namespace    https://github.com/KervynH/Simplify-Bilibili/raw/main/auto-wide-screen.user.js
+// @description  打开视频页面自动切换宽屏播放器
+// @match        https://www.bilibili.com/video/*
+// @run-at       document-idle
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    function clickWide() {
+        const btn = document.querySelector('[aria-label="宽屏"]');
+        if (!btn) return;
+        if (btn.dataset.__autoWideClicked) return;    // 防止疯狂连点
+
+        btn.dataset.__autoWideClicked = '1';
+        btn.click();
+    }
+
+    clickWide();
+
+    const observer = new MutationObserver(clickWide);
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
